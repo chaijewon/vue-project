a1309bbcace12d7f7bb83df7708f94153180910f
chaijewon <vcandjava@nate.com>
2025-12-02 12:47:31 +0900

    1111

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,21 @@
         <div class="row">
             <div class="col-md-3" v-for="(vo,index) in food_list" :key="index">
                 <div class="thumbnail">
-                    <a href="#">
+                    <router-link :to="{name:'food_detail',params:{fno:vo.fno}}">
                         <img :src="vo.poster" style="width:240px;height: 160px">
                         <div class="caption">
                         <p>{{vo.name}}</p>
                         </div>
-                    </a>
+                    </router-link>
                 </div>
             </div>
+        </div>
+        <div class="row text-center" style="margin-top: 10px">
+            <ul class="pagination">
+                <li v-if="startPage>1"><a class="a-link" @click="prev(startPage-1)">&lt;</a></li>
+                <li v-for="(i,index) in range(startPage,endPage)" :class="i===curpage?'active':''" :key="index"><a class="a-link" @click="pageChange(i)">{{i}}</a></li>
+                <li v-if="endPage<totalpage"><a class="a-link" @click="next(endPage+1)">&gt;</a></li>
+            </ul>
         </div>
     </div>
 </template>
@@ -44,9 +51,38 @@ export default({
                 const result=await response.json()
                 console.log(result)
                 this.food_list=result.list
+                this.curpage=result.curpage
+                this.totalpage=result.totalpage
+                this.startPage=result.startPage
+                this.endPage=result.endPage
            }catch(error){
               console.log(error)
            }
+        },
+        range(start,end){
+            let arr=[]
+            let len=end-start
+            for(let i=0;i<=len;i++)
+            {
+               arr[i]=start
+               start++
+            }
+            return arr
+        },
+        prev(page)
+        {
+            this.curpage=page
+            this.dataRecv()
+        },
+        next(page)
+        {
+            this.curpage=page
+            this.dataRecv()
+        },
+        pageChange(page)
+        {
+            this.curpage=page
+            this.dataRecv()
         }
     }
 })
@@ -61,4 +97,7 @@ export default({
     white-space: nowrap;
     text-overflow: ellipsis;
  }
-</style>
+ .a-link{
+    cursor: pointer;
+ }
+ </style>
